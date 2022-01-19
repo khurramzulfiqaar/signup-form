e189d2e96f26a9988c57f88cfb3ae677020e6780
khurramzulfiqaar <97911096+khurramzulfiqaar@users.noreply.github.com>
2022-01-19 22:32:01 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,76 @@
+import React from "react"
+
+export default function App() {
+    
+    const [formData, setFormData] = React.useState(
+        {
+            emailAddress: "",
+            password: "",
+            confirmPassword: "",
+            newsletterSignUp: false
+        }
+    )
+
+    function handleChange(event) {
+        const {name, value, type, checked} = event.target
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [name] : type === "checkbox" ? checked : value
+            }
+        })
+    }
+    
+    function handleSubmit(event) {
+        event.preventDefault()
+    }
+
+    
+    return (
+        <div className="form-container">
+            <form className="form" onSubmit={handleSubmit}>
+                <input 
+                    type="email" 
+                    placeholder="Email address"
+                    className="form--input"
+                    name="emailAddress"
+                    value={formData.emailAddress}
+                    onChange={handleChange}
+                />
+                <input 
+                    type="password" 
+                    placeholder="Password"
+                    className="form--input"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                />
+                <input 
+                    type="password" 
+                    placeholder="Confirm password"
+                    className="form--input"
+                    name="confirmPassword"
+                    value={formData.confirmPassword}
+                    onChange={handleChange}
+                />
+                
+                <div className="form--marketing">
+                    <input
+                        id="okayToEmail"
+                        type="checkbox"
+                        name="newsletterSignUp"
+                        checked={formData.newsletterSignUp}
+                        onChange={handleChange}
+                        
+                    />
+                    <label htmlFor="okayToEmail">I want to join the newsletter</label>
+                </div>
+                <button 
+                    className="form--submit"
+                >
+                    Sign up
+                </button>
+            </form>
+        </div>
+    )
+}
